fff16f107cacb60cad0e4ffd056f6d9fe0e41812
mkmtantip <mkmtan@tip.edu.ph>
2024-07-03 16:25:51 +0800

    just experimental

--- plzgod.py
+++ plzgod.py
@@ -6,8 +6,9 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 import cv2
 from pandas import *
 from Levenshtein import distance
+from nltk.corpus import stopwords
 
-
+stop_words = set(stopwords.words('english'))
 #Excel database to dict 
 xls = ExcelFile('Test.xlsx')
 df = xls.parse(xls.sheet_names[0])
@@ -23,27 +24,34 @@ def imageText():
     global output
     output = pytesseract.image_to_string(img)
     tokenizer()
-        
+
 def tokenizer():
     global processed
     unprocessed = re.split(r"[^a-zA-Z]", output) #just splits into spaces 
-    removeSpace = [ele for ele in unprocessed if ele.strip()] #removes spaces
+    removeSwords = [w for w in unprocessed if not w.lower() in stop_words] #removes stop words
+    # removes stop words remove words less than 3 letters remove words without vowels remove words with digits remove special 
+    # unprocessed = re.split(r'\b(\w*[\d]+\w*)|\W+|\d|\b\w{1,2}\b|\w*\d\w*|[^aeiou]+$', output) #just splits into spaces 
+    # removeSwords = [w for w in unprocessed if w is not None and not w.lower() in stop_words] #removes stop words
+    # do not remove word with digits, only remove digits in the word, also remove only digits 
+    removeSpace = [ele for ele in removeSwords if ele.strip()] #removes spaces
     processed = [x.lower() for x in removeSpace]
     print(processed)
     compare()
-    
+
+#try implement autocorrect
 def compare():
     for key, value in database.items():
         if key in processed:
             print("Term:", key,"Definition:", database[key],"Pronounciation: ", database2[key])
-            
-while True:
-    try:
-        if keyboard.is_pressed('q'):
-            screenshot = ImageGrab.grab()
-            screenshot.save("screenshot.png")
-            screenshot.close()
-            imageText()
-            break
-    except:
-        break
+#Testing
+imageText()
+# while True:
+#     try:
+#         if keyboard.is_pressed('q'):
+#             screenshot = ImageGrab.grab()
+#             screenshot.save("screenshot.png")
+#             screenshot.close()
+#             imageText()
+#             break
+#     except:
+#         break
---
+++ test321321.py
@@ -0,0 +1,36 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+# def clean_text(text):
+#     # Pattern to match symbols, two-letter words, and singular letter words
+#     pattern = r'\W+|\b\w{1,2}\b'
+    
+#     # Replace matched patterns with an empty string
+#     cleaned_text = re.sub(pattern, '', text)
+    
+#     return cleaned_text
+
+stop_words = set(stopwords.words('english'))
+
+# def removeDigits(text):
+#     pattern = '\w*\d\w*'
+#     cleaned_text = re.sub(pattern, '', text)
+#     return cleaned_text
+
+def tokenizer(text):
+    # global processed
+    # removed = removeDigits(original_text)
+    # print(removed)
+    unprocessed = re.split(r'[^a-zA-Z]', original_text) #just splits into spaces 
+    print(unprocessed)
+    removeSwords = [w for w in unprocessed if w is not None and not w.lower() in stop_words] #removes stop words
+    removeSpace = [ele for ele in removeSwords if ele.strip()] #removes spaces
+    processed = [x.lower() for x in removeSpace]
+    print(processed)
+    return processed
+
+# Example usage
+original_text = "Hello, World! It's a beautiful day. A quick brown fox jumps over the lazy dog. Nh Nh3213 Slayx22 nhkjkkkk fb ffs Slayyyyyy"
+processed = tokenizer(original_text)
+print(processed)
+
